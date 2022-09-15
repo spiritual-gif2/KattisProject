16e60d2b681c04c886eb9fc8f64bcda0bd8500ec
spiritual-gif2 <80268472+spiritual-gif2@users.noreply.github.com>
2022-09-15 10:03:22 +0200

    jetBrain test

--- Free_Food.py
+++ Free_Food.py
@@ -1,10 +1,10 @@
 freeFoodDays = []
 try:
     N = int(input())
-    if N in range(1,101):
+    if N in range(1, 101):
         for i in range(N):
-            s,t = [int(x) for x in input().split()]
-            eventDay = list(range(s,t+1))
+            s, t = [int(x) for x in input().split()]
+            eventDay = list(range(s, t + 1))
             for i in eventDay:
                 if i not in freeFoodDays:
                     freeFoodDays.append(i)
